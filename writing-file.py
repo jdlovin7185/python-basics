cd9e2bbec0c570404f54b1b651e74e9adce782b9
jdlovin <87381816+jdlovin@users.noreply.github.com>
2022-07-27 19:41:22 -0400

    writing to a file

---
+++ writing-file.py
@@ -0,0 +1,18 @@
+fhr = open("data.txt", "r")
+data = fhr.read()
+print("Before writing:")
+print(data)
+fhr.close()
+
+fhw = open("data.txt", "w")
+num = fhw.write("this new first line written\n")
+num1 = fhw.write("this new second line written\n")
+print("num:", num)
+print("num1:", num1)
+fhw.close()
+
+fhr = open("data.txt", "r")
+data = fhr.read()
+print("After Writing: ")
+print(data)
+fhr.close()
