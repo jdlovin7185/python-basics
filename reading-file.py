2506c2d38742549c8769f5dd21f8675a513b1ed8
jdlovin <87381816+jdlovin@users.noreply.github.com>
2022-07-27 19:19:49 -0400

    reading the whole document using a for loop

--- reading-file.py
+++ reading-file.py
@@ -1,3 +1,5 @@
+
+# for reading a file line by line, tedious process
 fhr = open("data.txt", "r")
 line1 = fhr.readline()
 print(line1, end="")
@@ -5,5 +7,12 @@ line2 = fhr.readline()
 print(line2, end="")
 line3 = fhr.readline()
 print(line3, end="")
+fhr.close()
+
+fhr = open("data.txt", "r")
+# reading literally the whole thing
+list_var = fhr.readlines()
+for line in list_var:
+    print(line, end="")
 fhr.close()
 
